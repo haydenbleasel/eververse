b73581ba87047586210b4114be2b124265920da8
Hayden Bleasel <hello@haydenbleasel.com>
2025-01-13 21:45:20 -0500

    Add legal links to footer for Slack app

--- apps/web/components/footer/index.tsx
+++ apps/web/components/footer/index.tsx
@@ -4,7 +4,7 @@ import { Logo } from '@repo/design-system/components/logo';
 import { Status } from '@repo/observability/status';
 import { FooterLink } from './footer-link';
 
-const legal = [
+const links = [
   {
     name: 'Home',
     href: '/',
@@ -23,26 +23,42 @@ const legal = [
   },
 ];
 
+const legal = [
+  {
+    name: 'Privacy Policy',
+    href: '/legal/privacy',
+  },
+  {
+    name: 'Terms of Service',
+    href: '/legal/terms',
+  },
+  {
+    name: 'Acceptable Use Policy',
+    href: '/legal/acceptable-use',
+  },
+];
+
 export const Footer = () => (
   <section className="relative overflow-hidden border-t">
-    <Container className="border-x p-4 text-center">
-      <div className="flex flex-col items-center justify-between gap-4 px-4 py-8 md:flex-row">
-        <div className="flex flex-col items-center gap-4 md:items-start">
-          <Link href="/">
-            <Logo showName />
-          </Link>
-          <p className="text-muted-foreground text-sm">
-            &copy; Eververse {new Date().getFullYear()}. All rights reserved.
-          </p>
-        </div>
-        <div className="flex flex-col items-center gap-4 md:items-end">
-          <div className="col-span-3 flex flex-wrap items-center justify-center gap-8">
-            {legal.map(({ href, name }) => (
-              <FooterLink key={name} href={href} name={name} />
-            ))}
-          </div>
-          <Status />
-        </div>
+    <Container className="grid grid-cols-4 items-start border-x px-4 py-8">
+      <div className="flex flex-col gap-4">
+        <Link href="/">
+          <Logo showName />
+        </Link>
+        <p className="text-muted-foreground text-sm">
+          &copy; Eververse {new Date().getFullYear()}. All rights reserved.
+        </p>
+        <Status />
+      </div>
+      <div className="col-start-3 flex flex-col gap-4">
+        {links.map(({ href, name }) => (
+          <FooterLink key={name} href={href} name={name} />
+        ))}
+      </div>
+      <div className="flex flex-col gap-4">
+        {legal.map(({ href, name }) => (
+          <FooterLink key={name} href={href} name={name} />
+        ))}
       </div>
     </Container>
   </section>
