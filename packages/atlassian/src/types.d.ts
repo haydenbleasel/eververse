cf1afc9ce2c9e84bffa85588125750d284036115
Hayden Bleasel <hello@haydenbleasel.com>
2025-01-15 10:32:27 -0500

    Update types.d.ts

--- packages/atlassian/src/types.d.ts
+++ packages/atlassian/src/types.d.ts
@@ -12369,6 +12369,31 @@ export interface paths {
         patch?: never;
         trace?: never;
     };
+    "/rest/api/3/workflows/search": {
+        parameters: {
+            query?: never;
+            header?: never;
+            path?: never;
+            cookie?: never;
+        };
+        /**
+         * Search workflows
+         * @description Returns a [paginated](#pagination) list of global and project workflows. If workflow names are specified in query string, details of those workflows are returned. Otherwise, all workflows are returned.
+         *
+         *     **[Permissions](#permissions) required:**
+         *
+         *      *  *Administer Jira* global permission to access all, including project-scoped, workflows
+         *      *  At least one of the *Administer projects* and *View (read-only) workflow* project permissions to access project-scoped workflows
+         */
+        get: operations["searchWorkflows"];
+        put?: never;
+        post?: never;
+        delete?: never;
+        options?: never;
+        head?: never;
+        patch?: never;
+        trace?: never;
+    };
     "/rest/api/3/workflows/update": {
         parameters: {
             query?: never;
@@ -15709,12 +15734,12 @@ export interface components {
         /** @description The current version details of this workflow scheme. */
         DocumentVersion: {
             /** @description The version UUID. */
-            id: string;
+            id?: string;
             /**
              * Format: int64
              * @description The version number.
              */
-            versionNumber: number;
+            versionNumber?: number;
         };
         DuplicatePlanRequest: {
             /** @description The plan name. */
@@ -24222,7 +24247,35 @@ export interface components {
              * @description The scope of the workflow. `GLOBAL` for company-managed projects and `PROJECT` for team-managed projects.
              * @enum {string}
              */
-            type: "PROJECT" | "GLOBAL";
+            type?: "PROJECT" | "GLOBAL";
+        };
+        /** @description Page of items, including workflows and related statuses. */
+        WorkflowSearchResponse: {
+            /** @description Whether this is the last page. */
+            isLast?: boolean;
+            /**
+             * Format: int32
+             * @description The maximum number of items that could be returned.
+             */
+            maxResults?: number;
+            /** @description If there is another page of results, the URL of the next page. */
+            nextPage?: string;
+            /** @description The URL of the page. */
+            self?: string;
+            /**
+             * Format: int64
+             * @description The index of the first item returned.
+             */
+            startAt?: number;
+            /** @description List of statuses. */
+            statuses?: components["schemas"]["JiraWorkflowStatus"][];
+            /**
+             * Format: int64
+             * @description The number of items returned.
+             */
+            total?: number;
+            /** @description List of workflows. */
+            values?: components["schemas"]["JiraWorkflow"][];
         };
         /** @description A workflow transition rule condition. This object returns `nodeType` as `simple`. */
         WorkflowSimpleCondition: {
@@ -54458,6 +54511,61 @@ export interface operations {
                 };
                 content: {
                     "application/json": components["schemas"]["WorkflowValidationErrorList"];
+                };
+            };
+            /** @description Returned if the request is not valid. */
+            400: {
+                headers: {
+                    [name: string]: unknown;
+                };
+                content?: never;
+            };
+            /** @description Returned if the authentication credentials are incorrect or missing, or the caller doesn't have permissions to perform the operation. */
+            401: {
+                headers: {
+                    [name: string]: unknown;
+                };
+                content?: never;
+            };
+        };
+    };
+    searchWorkflows: {
+        parameters: {
+            query?: {
+                /** @description The index of the first item to return in a page of results (page offset). */
+                startAt?: number;
+                /** @description The maximum number of items to return per page. */
+                maxResults?: number;
+                /** @description Use [expand](#expansion) to include additional information in the response. This parameter accepts a comma-separated list. Expand options include:
+                 *
+                 *      *  `values.transitions` Returns the transitions that each workflow is associated with. */
+                expand?: string;
+                /** @description String used to perform a case-insensitive partial match with workflow name. */
+                queryString?: string;
+                /** @description [Order](#ordering) the results by a field:
+                 *
+                 *      *  `name` Sorts by workflow name.
+                 *      *  `created` Sorts by create time.
+                 *      *  `updated` Sorts by update time. */
+                orderBy?: string;
+                /** @description The scope of the workflow. Global for company-managed projects and Project for team-managed projects. */
+                scope?: string;
+                /** @description Filters active and inactive workflows. */
+                isActive?: boolean;
+            };
+            header?: never;
+            path?: never;
+            cookie?: never;
+        };
+        requestBody?: never;
+        responses: {
+            /** @description Returned if the request is successful. */
+            200: {
+                headers: {
+                    [name: string]: unknown;
+                };
+                content: {
+                    "application/json": components["schemas"]["WorkflowSearchResponse"];
                 };
             };
             /** @description Returned if the request is not valid. */
