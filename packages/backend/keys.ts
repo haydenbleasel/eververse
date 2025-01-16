b2273cc876c37216012e51dc99e57985f8722dc4
Hayden Bleasel <hello@haydenbleasel.com>
2025-01-15 23:58:34 -0500

    Improve key validation

--- packages/backend/keys.ts
+++ packages/backend/keys.ts
@@ -7,8 +7,20 @@ export const keys = () =>
       SUPABASE_SERVICE_ROLE_KEY: z.string().min(1),
       SUPABASE_URL: z.string().url().min(1),
       SUPABASE_ANON_KEY: z.string().min(1),
-      POSTGRES_URL_NON_POOLING: z.string().url().min(1),
-      POSTGRES_PRISMA_URL: z.string().url().min(1),
+      POSTGRES_URL_NON_POOLING: z
+        .string()
+        .url()
+        .min(1)
+        .startsWith('postgresql://'),
+      POSTGRES_PRISMA_URL: z.string().url().min(1).startsWith('postgresql://'),
+
+      // This is a workaround key to get the prisma client to work with pgBouncer
+      // https://supabase.com/partners/integrations/vercel
+      PGBOUNCER_POSTGRES_PRISMA_URL: z
+        .string()
+        .url()
+        .min(1)
+        .startsWith('postgresql://'),
     },
     client: {
       NEXT_PUBLIC_SUPABASE_URL: z.string().url().min(1),
@@ -20,6 +32,7 @@ export const keys = () =>
       SUPABASE_ANON_KEY: process.env.SUPABASE_ANON_KEY,
       POSTGRES_URL_NON_POOLING: process.env.POSTGRES_URL_NON_POOLING,
       POSTGRES_PRISMA_URL: process.env.POSTGRES_PRISMA_URL,
+      PGBOUNCER_POSTGRES_PRISMA_URL: process.env.PGBOUNCER_POSTGRES_PRISMA_URL,
       NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
       NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
     },
--- packages/github/keys.ts
+++ packages/github/keys.ts
@@ -5,7 +5,10 @@ export const keys = () =>
   createEnv({
     server: {
       GITHUB_APP_ID: z.string().min(1),
-      GITHUB_PRIVATE_KEY: z.string().min(1),
+      GITHUB_PRIVATE_KEY: z
+        .string()
+        .min(1)
+        .startsWith('-----BEGIN PRIVATE KEY-----'),
       GITHUB_APP_SLUG: z.string().min(1),
     },
     runtimeEnv: {
