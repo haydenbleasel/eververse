05cc41a14b0b327a364f89b06c058acc0c0bffc6
Hayden Bleasel <hello@haydenbleasel.com>
2025-05-21 10:54:46 -0700

    Fix welcome page redirect

--- apps/app/app/sign-up/page.tsx
+++ apps/app/app/sign-up/page.tsx
@@ -4,37 +4,13 @@ import { Logo } from '@repo/design-system/components/logo';
 import { Prose } from '@repo/design-system/components/prose';
 import { createMetadata } from '@repo/seo/metadata';
 import type { Metadata } from 'next';
-import Image from 'next/image';
-import type { StaticImageData } from 'next/image';
 import Balancer from 'react-wrap-balancer';
 import { SignupForm } from './components/form';
-import ProductHunt from './product-hunt.svg';
 
 const title = 'Sign up';
 const description = 'Sign up to your account.';
 
 export const metadata: Metadata = createMetadata({ title, description });
-
-const Source = ({
-  name,
-  href,
-}: {
-  readonly name: string;
-  readonly href: string;
-}) => (
-  <Link href={href} className="text-foreground">
-    {name === 'producthunt' ? (
-      <Image
-        src={ProductHunt as StaticImageData}
-        alt="Product Hunt"
-        width={16}
-        height={16}
-        className="my-0 mr-1 inline h-4 w-4 rounded-full"
-      />
-    ) : null}
-    {name === 'producthunt' ? 'Product Hunt' : name}
-  </Link>
-);
 
 const SignUpPage = async () => {
   await handleAuthedState();
