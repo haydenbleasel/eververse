b518e4edb150fec0f11617beec4ce4e9d7d7c5bd
Hayden Bleasel <hello@haydenbleasel.com>
2025-05-21 13:28:21 -0700

    Update client.tsx

--- packages/analytics/posthog/client.tsx
+++ packages/analytics/posthog/client.tsx
@@ -5,18 +5,24 @@ import { PostHogProvider as PostHogProviderRaw } from 'posthog-js/react';
 import type { ReactNode } from 'react';
 import { keys } from '../keys';
 
-export const analytics = posthogRaw.init(keys().NEXT_PUBLIC_POSTHOG_KEY, {
-  api_host: '/ingest',
-  ui_host: keys().NEXT_PUBLIC_POSTHOG_HOST,
-  person_profiles: 'identified_only',
-  capture_pageview: false, // Disable automatic pageview capture, as we capture manually
-  capture_pageleave: true, // Overrides the `capture_pageview` setting
-}) as PostHog;
-
 type PostHogProviderProps = {
   readonly children: ReactNode;
 };
 
 export const PostHogProvider = (
   properties: Omit<PostHogProviderProps, 'client'>
-) => <PostHogProviderRaw client={analytics} {...properties} />;
+) => {
+  if (process.env.NODE_ENV === 'development') {
+    return properties.children;
+  }
+
+  const analytics = posthogRaw.init(keys().NEXT_PUBLIC_POSTHOG_KEY, {
+    api_host: '/ingest',
+    ui_host: keys().NEXT_PUBLIC_POSTHOG_HOST,
+    person_profiles: 'identified_only',
+    capture_pageview: false, // Disable automatic pageview capture, as we capture manually
+    capture_pageleave: true, // Overrides the `capture_pageview` setting
+  }) as PostHog;
+
+  return <PostHogProviderRaw client={analytics} {...properties} />;
+};
