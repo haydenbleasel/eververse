7621b417fffb44fbf6b8a83053b49e1ca0c1a27c
Hayden Bleasel <hello@haydenbleasel.com>
2025-05-21 02:17:43 -0700

    Remove Next.js monorepo workaround for Prisma

--- packages/next-config/index.ts
+++ packages/next-config/index.ts
@@ -1,11 +1,6 @@
 import withBundleAnalyzer from '@next/bundle-analyzer';
-
-// @ts-expect-error No declaration file
-import { PrismaPlugin } from '@prisma/nextjs-monorepo-workaround-plugin';
 import type { NextConfig } from 'next';
 import { createSecureHeaders } from 'next-secure-headers';
-
-const otelRegex = /@opentelemetry\/instrumentation/;
 
 export const config: NextConfig = {
   images: {
@@ -68,16 +63,6 @@ export const config: NextConfig = {
         destination: 'https://us.i.posthog.com/decide',
       },
     ];
-  },
-
-  webpack(config, { isServer }) {
-    if (isServer) {
-      config.plugins = [...config.plugins, new PrismaPlugin()];
-    }
-
-    config.ignoreWarnings = [{ module: otelRegex }];
-
-    return config;
   },
 
   // This is required to support PostHog trailing slash API requests
