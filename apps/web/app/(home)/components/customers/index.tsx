a9a7670b4285388b78d802f8d84333795afac620
Hayden Bleasel <hello@haydenbleasel.com>
2025-05-23 16:48:15 -0700

    Rework logos

--- apps/web/app/(home)/components/customers/index.tsx
+++ apps/web/app/(home)/components/customers/index.tsx
@@ -3,7 +3,6 @@
 import { Container } from '@repo/design-system/components/container';
 import { cn } from '@repo/design-system/lib/utils';
 import { LazyMotion, domAnimation, m, useInView } from 'motion/react';
-import Image from 'next/image';
 import { useRef } from 'react';
 import type { ComponentProps } from 'react';
 
@@ -11,19 +10,7 @@ type CustomersProperties = ComponentProps<'section'> & {
   readonly count: number;
 };
 
-import Cinemark from './logos/cinemark.svg';
-import Glide from './logos/glide.svg';
-import MeAndU from './logos/me&u.svg';
-import Monarch from './logos/monarch.svg';
-import Prismic from './logos/prismic.svg';
-
-const logos = [
-  { src: Cinemark, name: 'Cinemark' },
-  { src: Prismic, name: 'Prismic' },
-  { src: Glide, name: 'Glide Apps' },
-  { src: Monarch, name: 'Monarch Money' },
-  { src: MeAndU, name: 'me&u' },
-];
+import * as logos from './logos';
 
 export const Customers = ({
   className,
@@ -43,17 +30,17 @@ export const Customers = ({
       {...properties}
     >
       <LazyMotion features={domAnimation}>
-        <Container className="relative flex flex-col items-center justify-between gap-8 border-x py-8 sm:flex-row sm:gap-16">
+        <Container className="relative flex flex-col items-center justify-between gap-8 border-x py-16">
           <m.p
-            className="text-muted-foreground sm:max-w-xs"
+            className="text-muted-foreground"
             initial={{ opacity: 0 }}
             animate={{ opacity: inView ? 1 : 0 }}
             transition={{ duration: 0.5 }}
           >
             Join {closest}+ companies in building better products with Eververse
           </m.p>
-          <div className="grid grid-cols-3 gap-8 sm:grid-cols-5">
-            {logos.map(({ src, name }, index) => (
+          <div className="grid grid-cols-3 gap-8 text-foreground sm:grid-cols-5">
+            {Object.entries(logos).map(([name, Logo], index) => (
               <m.div
                 key={name}
                 className="flex items-center justify-center"
@@ -64,13 +51,7 @@ export const Customers = ({
                 }}
                 transition={{ duration: 0.5, delay: 0.1 * index }}
               >
-                <Image
-                  src={src}
-                  alt={name}
-                  width={96}
-                  height={24}
-                  className="h-6 w-24 object-contain dark:invert"
-                />
+                <Logo className="h-10 w-auto" />
               </m.div>
             ))}
           </div>
